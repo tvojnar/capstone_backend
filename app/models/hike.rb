7fa9ccbcbb662dcfd38b2c993ae14529e191a7a5
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-02 19:28:22 -0800

    added index route and hike.rb method to find all the hikes within a maps boundaries

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,15 +1,23 @@
 class Hike < ApplicationRecord
+  # database relationships:
   has_many :trackpoints
   has_many :waypoints
   has_many :points
 
+  # validations
   validates :name,
-          presence: true,
-          uniqueness: true
-
+  presence: true,
+  uniqueness: true
   validates :start_lat,
-            presence: true
-
+  presence: true
   validates :start_lng,
-            presence: true
+  presence: true
+
+  # method to return all the hikes that are within the boundaries of the map displayed on the react front end of the app
+  # min_lat, ect are passed in from the front end to the hikes controller
+  def self.search(min_lat:, max_lat:, min_lng:, max_lng:)
+    results = Hike.limit(100).where(start_lng: min_lng...max_lng, start_lat: max_lat...min_lat)
+    # binding.pry
+    return results
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # for now just have an index action, but later I will add show, create, edit, update actions as well 
+  scope :api do
+    resources :hikes, only: [:index]
+  end
 end
