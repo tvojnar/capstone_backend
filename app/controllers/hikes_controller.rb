bc089669c908cf809aaa370e51c1d1179a231e6b
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-07 10:50:22 -0800

    changed permitted params from medows to meadows so now that attribute can be changed from the frontend again

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -34,6 +34,6 @@ class HikesController < ApplicationController
   end
 
   def hike_params
-    params.require(:hike).permit(:name, :start_lat, :start_lng, :region, :description, :notes, :start_date, :end_date, :miles, :elevation_gain, :max_elevation, :coast, :rivers, :lakes, :waterfalls, :fall_foliage, :wildflowers, :medows, :mountain_views, :summits, :established_campsites, :day_hike, :overnight, :old_growth)
+    params.require(:hike).permit(:name, :start_lat, :start_lng, :region, :description, :notes, :start_date, :end_date, :miles, :elevation_gain, :max_elevation, :coast, :rivers, :lakes, :waterfalls, :fall_foliage, :wildflowers, :meadows, :mountain_views, :summits, :established_campsites, :day_hike, :overnight, :old_growth)
   end # hike_params
 end
