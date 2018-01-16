af086e428f934f7f4adf69193d881a7935af32d5
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-16 11:51:20 -0800

    added .aws/credentias to .gitignore

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 # to be able to generate presigned urls
 gem 'aws-sdk', '~> 2'
+# gem 'aws-sdk'
 # gem to be able to make post request to the browser
 gem 'rack-cors', :require => 'rack/cors'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,8 +6,14 @@
 class ImagesController < ApplicationController
 def index
     # see this resource https://stuff-things.net/2016/03/16/uploading-from-rails-to-aws-s3-with-presigned-urls/
-    credentials = Aws::Credentials.new('ID', 'KEY')
+    # id = ENV['AWS_ACCESS_KEY_ID']
+    # key = ENV['AWS_SECRET_ACCESS_KEY']
+    # credentials = Aws::Credentials.new('ID', 'KEY')
+    credentials = Aws::Credentials.new('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY')
+    # binding.pry
     s3_resource = Aws::S3::Resource::new(region: 'us-west-2', credentials: credentials, )
+    # s3_resource = Aws::S3::Resource::new(region: 'us-west-2', credentials: Aws::Credentials.new('access-key-id', 'secret-access-key'))
+    # s3_resource = Aws::S3::Resource::new(region: 'us-west-2')
     # pull out the file name from params
     # extension = File.extname(params[:filename])
     extension = params[:filename]
@@ -15,8 +21,8 @@ def index
     filename = "#{SecureRandom.uuid}.#{extension}"
     object = s3_resource.bucket('tv-capstone').object(filename)
     # get the url to pass to the client
-    # presigned_url = object.presigned_url(:put, content_type: params[:content_type] )
-    presigned_url = object.presigned_url(:put, expires_in: 5.minutes.to_i)
+    # presigned_url = object.presigned_url(:put)
+    presigned_url = object.presigned_url(:put, content_type: params[:content_type], expires_in: 5.minutes.to_i, acl: 'public-read')
     render json: {url: presigned_url }, status: :ok
     # binding.pry
   end
