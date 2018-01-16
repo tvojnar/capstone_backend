251e59b7a9a5353f27b6b0189e5ca0520b893a41
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-15 19:39:32 -0800

    added comments to Images#index

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,11 +5,15 @@
 
 class ImagesController < ApplicationController
   def index
+    # see this resource https://stuff-things.net/2016/03/16/uploading-from-rails-to-aws-s3-with-presigned-urls/
     credentials = Aws::Credentials.new('ID', 'KEY')
     s3_resource = Aws::S3::Resource::new(region: 'us-west-1', credentials: credentials)
+    # pull out the file name from params
     extension = File.extname(params[:filename])
+    #add a random number to it to make sure that the filename sent to S3 is unique
     filename = "#{SecureRandom.uuid}.#{extension}"
     object = s3_resource.bucket('tv-capstone').object(filename)
+    # get the url to pass to the client
     presigned_url = object.presigned_url(:put)
     render json: {url: presigned_url }, status: :ok
   end
