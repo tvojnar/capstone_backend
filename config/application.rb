8b9e081c361f5e0de773ba7a0bc64cd808841e49
Tamira Vojnar <tamiravojnar@gmail.com>
2018-01-23 13:36:40 -0800

    added create to CORS

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module CapstoneBackend
     config.middleware.insert_before 0, Rack::Cors do
      allow do
        origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :options, :patch, :delete]
+       resource '*', :headers => :any, :methods => [:get, :post, :options, :patch, :delete, :create]
      end
    end
   end
